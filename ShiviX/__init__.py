3bdf6ef54bfd815c1663a26a68c0e73c5e8366da
team-katil <115205040+team-katil@users.noreply.github.com>
2022-10-07 11:52:39 +0530

    Create __init__.py

---
+++ ShiviX/__init__.py
@@ -0,0 +1,41 @@
+from ShiviX.core.bot import AnonXBot
+from ShiviX.core.dir import dirr
+from ShiviX.core.git import git
+from ShiviX.core.userbot import Userbot
+from ShiviX.misc import dbb, heroku, sudo
+from aiohttp import ClientSession
+
+from .logging import LOGGER
+
+# Directories
+dirr()
+
+# Check Git Updates
+git()
+
+# Initialize Memory DB
+dbb()
+
+# Heroku APP
+heroku()
+
+# Load Sudo Users from DB
+sudo()
+
+# Bot Client
+app = ShiviXBot()
+
+# Assistant Client
+userbot = Userbot()
+
+from .platforms import *
+
+YouTube = YouTubeAPI()
+Carbon = CarbonAPI()
+Spotify = SpotifyAPI()
+Apple = AppleAPI()
+Resso = RessoAPI()
+SoundCloud = SoundAPI()
+Telegram = TeleAPI()
+
+aiohttpsession = ClientSession()
