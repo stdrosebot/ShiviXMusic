a28f2251959d8256c5548a7180b9a7e710a695ea
team-katil <115205040+team-katil@users.noreply.github.com>
2022-10-25 16:15:52 +0530

    Update admins.py

--- ShiviX/utils/decorators/admins.py
+++ ShiviX/utils/decorators/admins.py
@@ -75,7 +75,7 @@ def AdminActual(mystic):
         if await is_maintenance() is False:
             if message.from_user.id not in SUDOERS:
                 return await message.reply_text(
-                    "Bot is under maintenance. Please wait for some time. Otherwise visit support chat."
+                    "Bot is under maintenance. Please wait for some time. Otherwise visit support chat.",
                 )
         if await is_commanddelete_on(message.chat.id):
             try:
